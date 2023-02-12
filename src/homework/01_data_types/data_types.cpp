44faeee1a3d3e7ff0254696101dd2f291b3f6e85
Afaq Ahmad <afaqaijaz66@gmail.com>
2023-02-12 05:10:13 +0000

    afaqahmadijaz

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -1,13 +1,17 @@
 #include "data_types.h"
 
 //example
-int add_numbers(int num1, int num2)
+/*int add_numbers(int num1, int num2)
 {
 	return num1 + num2;
-}
+}*/
 
 //write function code here
 
-
+int multiply_numbers(int num1)
+{
+	int num2 = 5;
+	return num1 * num2;
+	}
 
 
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,21 @@
 ﻿//write include statements
-
+#include <iostream>
+#include "data_types.cpp"
 
 //write namespace using statement for cout
 
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int num1;
+    cout << "Enter a number: ";
+    cin >> num1;
+	int result = multiply_numbers(num1);
+	cout << "The result of multiplying the number with 5 is: " << result << endl;
 
 	return 0;
 }
