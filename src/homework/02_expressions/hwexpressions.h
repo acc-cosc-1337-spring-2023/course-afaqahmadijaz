310dcb97df0b4e6fbb32f0d0b645a262e75fc333
Afaq Ahmad <afaqaijaz66@gmail.com>
2023-02-12 05:09:58 +0000

    afaqahmadijaz

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,13 @@
 ﻿//example
 int add_numbers(int num1, int num2);
 
+
 //write function prototype here
 
+#ifndef HWEXPRESSIONS_H
+#define HWEXPRESSIONS_H
 
+double get_sales_tax_amount(double meal_amount);
+double get_tip_amount(double meal_amount, double tip_rate);
 
-
+#endif
